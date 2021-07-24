8bb02c746da4272819d604f0ed93b039dec23c31
asima-chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2021-07-24 22:16:24 +0600

    done

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 interface IProps {
     name: string,
     email?: string,
+    handleRemove: (email:string) => void
 }
 
-const Contact = ({ name, email = "N/A" }: IProps) : JSX.Element => {
+const Contact = ({ name, email = "N/A" ,handleRemove}: IProps) : JSX.Element => {
     return (
         <div className="card">
             <p>
@@ -14,22 +15,23 @@ const Contact = ({ name, email = "N/A" }: IProps) : JSX.Element => {
             <p>
                 <strong>Email: </strong> {email}
             </p>
+            <button onClick={()=>handleRemove(email)}>Remove</button>
         </div>
     );
 };
 
 // export const ContactDemo: React.FunctionComponent<IProps> = ({ name, email = "N/A" }: IProps) => {
-export const ContactDemo: React.FC<IProps> = ({ name, email = "N/A" }: IProps) => {
-    return (
-        <div className="card">
-            <p>
-                <strong>Name: </strong> {name}
-            </p>
-            <p>
-                <strong>Email: </strong> {email}
-            </p>
-        </div>
-    );
-};
+// export const ContactDemo: React.FC<IProps> = ({ name, email = "N/A" }: IProps) => {
+//     return (
+//         <div className="card">
+//             <p>
+//                 <strong>Name: </strong> {name}
+//             </p>
+//             <p>
+//                 <strong>Email: </strong> {email}
+//             </p>
+//         </div>
+//     );
+// };
 
 export default Contact;
--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,41 +1,64 @@
 import React, { useState } from 'react';
-import Contact,{ContactDemo} from './Contact';
+import Contact from './Contact';
 
+interface IContact {
+    name: string,
+    email: string,
+}
 const Contacts = () => {
-    const [contact, setContact] =useState('');
-    const [contactList, setContactList] = useState<string[]> ([]); 
+    // const [contact, setContact] = useState<IContact>({} as IContact);
+    const [contact, setContact] = useState<IContact>({
+        name: "",
+        email: ""
+    });
+    const [contactList, setContactList] = useState<IContact[]>([]);
 
     console.log('contact', contact);
 
-    const onClick =()=>{
+    const onClick = () => {
         setContactList([...contactList, contact]);
-        setContact("");
+        setContact({
+            name: '',
+            email: ''
+        });
     }
-    console.log('contactList',contactList);
+    console.log('contactList', contactList);
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+        setContact({
+            ...contact,
+            [e.target.name]: e.target.value
+        });
+    const handleRemove = (email: string) =>{
+        const newContactList = contactList.filter(c => c.email !== email);
+        setContactList(newContactList);
+    }
     return (
         <div>
             <h1>Contact List</h1>
             <div className="form">
-                <input 
-                    value={contact}
-                    onChange={(e)=> setContact(e.target.value)}
-                    name="name" placeholder="Contact Name" type="text"
-                 />
-                 <button onClick={onClick}>Add</button>
+                <input
+                    value={contact.name}
+                    onChange={onChange}
+                    name="name"
+                    placeholder="Contact Name"
+                    type="text"
+                />
+                <input
+                    value={contact.email}
+                    onChange={onChange}
+                    name="email"
+                    placeholder="Contact Email"
+                    type="email"
+                />
+                <button onClick={onClick}>Add</button>
             </div>
             {
-                contactList.map((contactName) => 
-                    <Contact name={contactName} key={contactName} />
+                contactList.map((con) =>
+                    <Contact key={con.name} name={con.name} email={con.email} handleRemove={handleRemove} />
                 )
             }
-            <Contact name="Someone 1">
-             <h1>Children</h1>   
-            </Contact>
-           <ContactDemo name="Someone 2">
-             <h1>Children</h1>   
-            </ContactDemo> 
-            {/*        
-            <Contact name="sima"/> */}
+            {/* <Contact name="sima"/> */}
         </div>
     );
 };
